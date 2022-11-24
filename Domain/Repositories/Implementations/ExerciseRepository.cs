5470f72b66397a5a8495abf9333f680e4a4a52c5
trueberryless <felix@schneider-it.at>
2022-11-24 14:49:39 +0100

    Profile Page

--- Domain/Repositories/Implementations/ExerciseRepository.cs
+++ Domain/Repositories/Implementations/ExerciseRepository.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Model.Configurations;
 using Model.Entities;
 
 namespace Domain.Repositories.Implementations;
 
-public class ExerciseRepository : ARepository<Exercise>
+public class ExerciseRepository : ARepository<Exercise>, IExerciseRepository
 {
     public ExerciseRepository(TrainITDbContext context) : base(context)
     {
